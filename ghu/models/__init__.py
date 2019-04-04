f8fd8e7ae4c0c29387a1e30a82726f5c2a456e77
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-04-04 19:54:08 +0200

    Remove application

--- ghu/models/__init__.py
+++ ghu/models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import advisor
-from . import application
+#from . import application
 from . import program
-from . import student
+#from . import student
 from . import study
-from . import university
+#from . import university
