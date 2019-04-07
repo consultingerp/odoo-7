e0863b8fde783480087b5671e539ccdf5771c8fa
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-04-07 21:29:37 +0200

    [TASK] default sorting advisor

--- ghu/models/advisor.py
+++ ghu/models/advisor.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class GhuAdvisor(models.Model):
     _name = 'ghu.advisor'
     _description = "Advisor"
-
+    _order = "nomination.name DESC, nationality.name ASC, last_name ASC"
     _inherits = {"res.partner": "partner_id"}
 
     advisor_id = fields.Char('Advisor ID', size=12)
