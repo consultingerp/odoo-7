6a4d332e58b2948f82fbd18b263c749ce9b903cc
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-04-10 18:28:52 +0200

    [ORDERING]

--- ghu/models/employee.py
+++ ghu/models/employee.py
@@ -5,4 +5,7 @@ from odoo import models, fields, api
 class GhuEmployee(models.Model):
  _inherit = 'hr.employee'
  # we inherited res.groups model which is Odoo/OpenERP built in model and created two fields in that model.
+ _order = "website_order"
+
  skype = fields.Char(string="Skype")
+ website_order = fields.Integer(string="Order")
