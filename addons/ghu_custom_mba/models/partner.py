00bd34979cc2a6f1212960f5bbf67da71e407b97
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-08-05 17:50:52 +0200

    [FIX] prevent error through correct permission checks

---
+++ addons/ghu_custom_mba/models/partner.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import models, fields, api
+
+
+_logger = logging.getLogger(__name__)
+
+
+class GhuPartner(models.Model):
+    _inherit = 'res.partner'
+
+    is_advisor = fields.Boolean(
+        string=u'Is advisor?',
+        compute='_is_advisor'
+    )
+
+    is_custom_mba = fields.Boolean(
+        string=u'Is custom MBA?',
+        compute='_is_custom_mba'
+    )
+
+    @api.multi
+    def _is_advisor(self):
+        for record in self:
+            if self.env['ghu.advisor'].sudo().search([('partner_id', '=', record.id)]):
+                record.is_advisor = True
+            else:
+                record.is_advisor = False
+
+    @api.multi
+    def _is_custom_mba(self):
+        for record in self:
+            if (record._is_advisor()):
+                advisor = self.env['ghu.advisor'].sudo().search(
+                    [('partner_id', '=', record.id)], limit=1)
+                if advisor.is_cafeteria:
+                    record.is_custom_mba = True
+                else:
+                    record.is_custom_mba = False
