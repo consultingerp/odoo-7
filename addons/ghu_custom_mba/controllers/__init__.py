ef60db89854e403c9640a82f995f0c9e127e7f89
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-08-27 22:21:25 +0200

    Fix some weird things

--- addons/ghu_custom_mba/controllers/__init__.py
+++ addons/ghu_custom_mba/controllers/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from . import blti
 from . import courses
 from . import documents
 from . import web
--- addons/ghu_custom_mba/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import panopto
-from . import blti
