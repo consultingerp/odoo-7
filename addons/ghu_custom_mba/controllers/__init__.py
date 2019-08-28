a0876f636b2d1acbdce41c469a0ec98da56c3532
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-08-28 12:49:25 +0200

    Fix init files

--- addons/ghu_custom_mba/controllers/__init__.py
+++ addons/ghu_custom_mba/controllers/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from . import blti
 from . import courses
 from . import documents
 from . import web
--- addons/ghu_custom_mba/models/__init__.py
+++ addons/ghu_custom_mba/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from . import panopto
 from . import course
 from . import res_config_settings
 from . import partner
