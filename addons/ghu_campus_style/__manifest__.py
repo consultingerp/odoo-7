55e11c835f82bbb1f682a1bd73557fe6c3f21d79
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-09-07 15:47:26 +0200

    C'd

--- addons/ghu_campus_style/__manifest__.py
+++ addons/ghu_campus_style/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.14',
+    'version': '0.15',
 
     # any module necessary for this one to work correctly
     'depends': ['web'],
@@ -26,11 +26,12 @@
         'views/common/layout.xml',
         'views/common/assets.xml',
         'views/common/header.xml',
-        'views/navigation/navbars.xml'
+        'views/navigation/navbars.xml',
+        'views/navigation/sidebars/account.xml'
     ],
     # only loaded in demonstration mode
-    #'demo': [
+    # 'demo': [
     #    'demo/demo.xml',
-    #],
+    # ],
     'application': True
 }
