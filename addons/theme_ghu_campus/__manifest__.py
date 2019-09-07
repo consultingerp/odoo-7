32e0b9db3b1eb57cfeb1ba26a1326bb100429304
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-09-07 11:34:48 +0200

    add dependency

--- addons/theme_ghu_campus/__manifest__.py
+++ addons/theme_ghu_campus/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['web'],
+    'depends': ['website_theme_install', 'web'],
 
     # always loaded
     'data': [
