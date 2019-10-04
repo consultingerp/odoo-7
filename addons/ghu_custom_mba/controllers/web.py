6627ade4b4a74db1ef3bf4c9e6c81e8c875ca671
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-10-04 18:37:28 +0200

    [HOTFIX] do not redirect from startpage when custom mba logged in

--- addons/ghu_custom_mba/controllers/web.py
+++ addons/ghu_custom_mba/controllers/web.py
@@ -8,11 +8,11 @@ from odoo.http import request
 
 class Home(Home):
 
-    @http.route()
-    def index(self, *args, **kw):
-        if request.session.uid and request.env.user.partner_id.is_custom_mba:
-            return http.local_redirect('/campus/my/courses', query=request.params, keep_hash=True)
-        return super(Home, self).index(*args, **kw)
+    #@http.route()
+    #def index(self, *args, **kw):
+    #    if request.session.uid and request.env.user.partner_id.is_custom_mba:
+    #        return http.local_redirect('/campus/my/courses', query=request.params, keep_hash=True)
+    #    return super(Home, self).index(*args, **kw)
 
     def _login_redirect(self, uid, redirect=None):
         if not redirect and request.env.user.partner_id.is_custom_mba:
