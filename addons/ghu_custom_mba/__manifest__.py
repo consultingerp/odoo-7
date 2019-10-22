6929db1bc4c1fe51c23c52842291faf92bb540c2
geraldaistleitner <gerald.aistleitner@gmail.com>
2019-10-22 23:30:45 +0200

    Fix new course error (#15)
    * Change menu entry names
    
    * Edit page refactoring in progress
    
    * Begin Final work
    
    * C'd
    
    * Add mollie as payment method
    
    * start adding assessments
    
    * add mollie to manifest
    
    * [ADD] submodule addons/mollie
    
    * Add requirements
    
    * Move logo in mail signature
    
    * selected has to be present
    
    * fix selected without value
    
    * fix
    
    * last fix
    
    * Fix this
    
    * Raise versions
    
    * This should work
    
    * Raise things up
    
    * Start creation of assessment
    
    * Add assessments, refactor views
    
    * Fix error on installation
    
    * Progress
    
    * Fix links
    
    * Fix lt; sign
    
    * Fix stretched link
    
    * Assessment questions
    
    * Fix
    
    * question edit
    
    * Fixes
    
    * Minor fix
    
    * Small fix
    
    * Raise version
    
    * Fixes

--- addons/ghu_custom_mba/__manifest__.py
+++ addons/ghu_custom_mba/__manifest__.py
@@ -16,7 +16,9 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.42',
+
+    'version': '0.43',
+
 
     # any module necessary for this one to work correctly
     'depends': ['ghu', 'documents'],
