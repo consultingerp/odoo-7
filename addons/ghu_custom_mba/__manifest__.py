2ce9bbaef6da9700ff78dda8ec72ec6454ebc35e
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-10-22 22:42:59 +0200

    Fix

--- addons/ghu_custom_mba/__manifest__.py
+++ addons/ghu_custom_mba/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.37',
+    'version': '0.38',
 
     # any module necessary for this one to work correctly
     'depends': ['ghu', 'documents'],
@@ -33,6 +33,7 @@
         'views/course/edit.xml',
         'views/course/record.xml',
         'views/course/assessments/edit.xml',
+        'views/course/assessments/question/edit.xml',
         'views/video/myvideos.xml',
         'views/video/panopto.xml',
         'views/documents/list.xml',
