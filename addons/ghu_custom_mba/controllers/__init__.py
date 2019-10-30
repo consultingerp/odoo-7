22c66702d471bb955f5182bbf8f6d3736efe8420
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-10-30 19:43:21 +0100

    Fix

--- addons/ghu_custom_mba/controllers/__init__.py
+++ addons/ghu_custom_mba/controllers/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import application
-from . import courses-advisor
-from . import courses-student
+from . import courses_advisor
+from . import courses_student
 from . import documents
 from . import web
