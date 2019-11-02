1fae871f1eaf2efa5d2af6ffb798dec5de42322a
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-11-02 15:23:01 +0100

    Buy course

--- addons/ghu_custom_mba/controllers/courses_student.py
+++ addons/ghu_custom_mba/controllers/courses_student.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import datetime
 import logging
 import json
 import base64
@@ -36,3 +37,37 @@ class GhuCustomMbaStudent(http.Controller):
             partner_id = request.env.user.partner_id.id
             # TODO: Get all courses a partner/student bought
         return http.request.not_found()
+
+    @http.route('campus/course/buy/<model("ghu_custom_mba.course"):obj>', auth='user', website=True)
+    def buyCourse(self, obj, **kw):
+        invoice_partners = request.env.user.partner_id.child_ids.filtered(lambda p: p.type == 'invoice')
+        invoice = self.env['account.invoice'].create(dict(
+            partner_id=invoice_partners[0].id if invoice_partners else request.env.user.partner_id.id, # customer (billing address)
+            partner_shipping_id=request.env.user.partner_id.id, # customer (applicant)
+            type='out_invoice',
+            date_invoice=datetime.datetime.utcnow().date(), # invoice date
+            date_due=(datetime.datetime.utcnow() + datetime.timedelta(weeks=1)).date(), # due date
+            user_id=self.env().user.id, # salesperson
+            invoice_line_ids=[], # invoice lines
+            name=obj.product_ref.name, # name for account move lines
+            partner_bank_id=self.env['ir.config_parameter'].get_param('ghu.automated_invoice_bank_account'), # company bank account
+        ))
+
+        invoice_line = self.env['account.invoice.line'].with_context(
+                type=invoice.type, 
+                journal_id=invoice.journal_id.id, 
+                default_invoice_id=invoice.id
+            ).create(dict(
+                product_id=obj.product_ref.id,
+                name=obj.product_ref.name,
+                price_unit=obj.product_ref.lst_price,
+            ))
+
+        invoice.invoice_line_ids = [(4, invoice_line.id)]
+        invoice.action_invoice_open()
+        invoice_template = self.env.ref('ghu.ghu_invoice_email_template')
+        invoice_template.send_mail(invoice.id)
+        invoice.write({'sent': True})
+
+        return http.request.redirect('/my/invoices/'+invoice.id)
+
