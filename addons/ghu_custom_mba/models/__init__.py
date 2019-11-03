9e7f0a0533402582084f8e89a368d84151dafe2d
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-11-03 19:03:59 +0100

    Fix id and import

--- addons/ghu_custom_mba/models/__init__.py
+++ addons/ghu_custom_mba/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import advisor
 from . import course
+from . import course_enrollment
 from . import res_config_settings
 from . import partner
 from . import student
