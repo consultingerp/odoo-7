334a6fe30084d9f55b080c358e8129adfcb4dbd2
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2019-12-02 10:22:48 +0100

    Add icon to model

---
+++ addons/theme_campus/models/menu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import api, fields, models
+
+class CampusMenu(models.Model):
+    _inherit = 'theme.website.menu'
+
+    
+    icon = fields.Char(
+        string='Icon',
+    )
+    
