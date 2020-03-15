ec18705e8b509e8974030c7b0297478aa976a52e
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2020-03-15 11:54:19 +0100

    Proceed

--- addons/ghu_msc/__manifest__.py
+++ addons/ghu_msc/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.101',
+    'version': '0.102',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mail', 'ghu'],
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'data/activity_types.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/application/form.xml',
