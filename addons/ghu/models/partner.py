f23a1b7761e27879d13a89e2ac1dcfe532165f80
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2020-03-24 18:23:30 +0100

    Fix some issues

--- addons/ghu/models/partner.py
+++ addons/ghu/models/partner.py
@@ -8,5 +8,5 @@ _logger = logging.getLogger(__name__)
 class GhuPartner(models.Model):
     _inherit = 'res.partner'
 
-    interest_id = fields.Many2many('ghu.partner.interest', relation='ghu_partner_interest', column1='partner_id',
+    interest_id = fields.Many2many('ghu.partner.interest', relation='ghu_rel_partner_interest', column1='partner_id',
                                    column2='interest_id', string='Tags')
