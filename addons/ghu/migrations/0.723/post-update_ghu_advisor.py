86a4c29ebc49601d964574600b1cc40a87e016fe
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2020-03-27 20:54:14 +0100

    Migrate to partner

---
+++ addons/ghu/migrations/0.723/post-update_ghu_advisor.py
@@ -0,0 +1,6 @@
+def migrate(cr, version):
+    cr.execute('SELECT partner_id, vita_file, vita_filename FROM ghu_advisor WHERE vita_file IS NOT NULL')
+    advisors = cr.dictfetchall()
+    if advisors:
+        for advisor in advisors:
+            cr.execute('UPDATE res_partner SET vita_file="%s", vita_file_filename="%s" WHERE id = %s', [advisor['vita_file'], advisor['vita_filename'], advisor['partner_id']])
