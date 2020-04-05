38c409573d3473d0f7e08e47640f36875726984f
Gerald Aistleitner <gerald.aistleitner@gmail.com>
2020-04-05 08:08:37 +0200

    fix some things

--- addons/ghu/__manifest__.py
+++ addons/ghu/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.730',
+    'version': '0.731',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'base_automation', 'web', 'crm', 'account', 'website', 'website_form', 'website_partner',
